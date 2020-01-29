18031e12351745eaaaa23836093dd8d84da8bd1b
chltjdrhd777 <58500558+chltjdrhd777@users.noreply.github.com>
2020-01-29 15:46:32 +0900

    ok

---
+++ dist/project1.js
@@ -0,0 +1,17 @@
+function add(n1, n2, showResult, phrase) {
+    //const result = n1 + n2;
+    if (showResult) {
+        //console.log(phrase + n1+ n2) -> If I write codes like this, it's result would be 'the answer is 51.2' because it would put one string and two numbers together as string.
+        // to avoid this, I have two options. One : regulate the calculation as constant separately. Second, add change n1+n2 into Number(n1+n2)
+        console.log(phrase + Number(n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
+}
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is:";
+add(number1, number2, printResult, resultPhrase);
+//# sourceMappingURL=project1.js.map
